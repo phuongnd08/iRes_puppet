7e7dd89f59c8c7e27dffa1b018071d7bb6dff049
Phuong Nguyen <phuongnd08@gmail.com>
2011-11-05 16:21:20 +0700

    Add a script to validate all puppets and templates

---
+++ verify_puppets_and_templates.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+find modules manifests -name '*.pp' \
+  | xargs -t -n1 -P2 sh -c 'puppet --parseonly "$@" || exit 255' -
+
+find modules/*/templates/ -maxdepth 1 -type f -not -name .svn \
+  | xargs -n1 sh -c 'erb -x -T - $1 | ruby -c 2>&1 | sed -e "s,^,$1: ,"' -
