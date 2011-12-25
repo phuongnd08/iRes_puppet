d1d71a9f48575b5a4b96ab9666ff98b8b85339f8
Phuong Nguyen <phuongnd08@gmail.com>
2011-12-25 17:50:39 +0700

    Run generic script/cron rather than refresh user score

--- modules/cron/manifests/init.pp
+++ modules/cron/manifests/init.pp
@@ -2,7 +2,7 @@ class cron {
   $working_dir = '/home/root/giasu_app/current'
   cron {
     refresh_user_score:
-      command => "/usr/bin/env PATH=${env_path} /bin/sh -l -c 'cd ${working_dir} && bundle exec script/refresh_user_score'",
+      command => "/usr/bin/env PATH=${env_path} /bin/sh -l -c 'cd ${working_dir} && bundle exec script/cron'",
       hour => 0,
       minute => 1;
     backup_mysql:
