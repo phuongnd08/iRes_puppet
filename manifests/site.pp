d1a256d5c9d36deb7c2e91b9602964fda7fd374c
Phuong Nguyen <phuongnd08@gmail.com>
2012-04-07 23:29:18 +0700

    Add rvm

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,13 @@
 node default {
   include monit
   include sshd
-  include nginx
   include mysql
+  include rvm
+  rvm_system_ruby {
+    'ruby-1.9.3-p0':
+      ensure => 'present',
+      default_use => true;
+  }
   #include push_server
   #include redis
   #include resque
