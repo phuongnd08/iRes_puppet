7c992c056c0a7e42b532beecaeb7d5b4fbbcf66f
deploy <deploy@ubuntu-ires.(null)>
2012-04-30 12:09:07 -0400

    Add the one-shot install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+apt-get update
+apt-get install -y git-core puppet
+git clone git://github.com/phuongnd08/iRes_puppet.git
+cd iRes_puppet
+git submodule update --init
+puppet apply --modulepath ./modules manifests/site.pp
+puppet apply --modulepath ./modules manifests/site.pp
