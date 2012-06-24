7b10967fdf4b5070b53e5e846eadc7c042bb869e
Phuong Nguyen <phuongnd08@gmail.com>
2012-06-24 06:55:01 -0400

    Embed the key inside puppet recipe since template is adding newline to the key

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -6,20 +6,14 @@ class sshd{
 
   monit::package { "sshd": }
 
-  ssh_authorized_key {
-    "phuongnd08@ubuntu-server":
-      ensure => "present",
-      type => "ssh-rsa",
-      key => template("sshd/deploy_key_phuong_desktop.erb"),
-      target => "/home/deploy/.ssh/authorized_keys",
-      user => "deploy"
-  }
+  $phuong_laptop_key = "AAAAB3NzaC1yc2EAAAADAQABAAABAQDL4gLxbzOxFtNXQxCXYA3SKin9Q5t/Onngi8SUiJnu2EhTQEOX//RUPpwtWwSUnNyOueEIfjtCPd/A1IG3YWTawD8OL14ZprciWZuYBtjj1PkEbZByq5PHdyENp+kVw8QspS3f+xViSLG2gW026pj7YU7kBKEvTIc7/ZPbnUYDWU528ARyVoq1lfbmmg0Jbc3szL3VIYQwclJtClkx7N4UX56ESAFTw6aHlsgzV993WjVq4KT7BwwKbv7MKJjNr3MlU76QjWmN4MxWxH+9bVj6QzdzB246/4SfHwZlUK/j+LHFzv2pFH/0Q6jLrMqdpvT+Hgy6caQ6V3CXsbBo5ayj"
+  $github_deploy_key = "AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
 
   ssh_authorized_key {
     "phuongnd08@laptop":
       ensure => "present",
       type => "ssh-rsa",
-      key => template("sshd/deploy_key_phuong_laptop.erb"),
+      key => $phuong_laptop_key,
       target => "/home/deploy/.ssh/authorized_keys",
       user => "deploy"
   }
@@ -28,7 +22,7 @@ class sshd{
     "github.com":
       ensure => "present",
       type => "ssh-rsa",
-      key => template("sshd/github_key.erb")
+      key => $github_deploy_key
   }
 
   file { "/etc/ssh/ssh_known_hosts":
