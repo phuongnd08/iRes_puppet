33b266c55bb5dfdab4168ea2c8270feef04fde78
phuongnd08 <phuongnd08@gmail.com>
2012-06-24 17:33:04 +0700

    Update install.sh: Change the git url to https to be able to fill in username and password

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 apt-get update
 apt-get install -y git-core puppet
-git clone git://github.com/phuongnd08/iRes_puppet.git
+git clone https://github.com/phuongnd08/iRes_puppet.git
 cd iRes_puppet
 git submodule update --init
 puppet apply --modulepath ./modules manifests/site.pp
